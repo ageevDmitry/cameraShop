8ee705ea3cfefc8d61035cf8b3827825a50ea920
ageevDmitry <ageevdmitry2018@gmail.com>
2023-01-28 09:05:21 +0300

    Добавляет переход по пагинации

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,3 +1,7 @@
+import {useAppDispatch} from '../../hooks';
+import {changeCurrentCatalogPage} from '../../store/products-ui/products-ui';
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
 
 type PaginationProps = {
   paginationCount: number;
@@ -6,13 +10,20 @@ type PaginationProps = {
 
 function Pagination ({paginationCount, currentCatalogPage}: PaginationProps): JSX.Element {
 
+  const dispatch = useAppDispatch();
   const paginationItems = Array.from({length: paginationCount}, (_, index) => index + 1);
 
   return (
     <div className="pagination">
       <ul className="pagination__list">
         {paginationItems.map((item) => (
-          <li key={item} className="pagination__item"><a className="pagination__link pagination__link--active" href="/">{item}</a>
+          <li key={item} className="pagination__item">
+            <Link to = {AppRoute.Catalog} className={`pagination__link ${(item === currentCatalogPage) ? 'pagination__link pagination__link--active' : ''}`}
+              onClick={() => {
+                dispatch(changeCurrentCatalogPage({page: item}));
+              }}
+            >{item}
+            </Link>
           </li>
         ))}
       </ul>
