5a638cb75863763d63fc965bbab3707f40f60bf7
ageevDmitry <ageevdmitry2018@gmail.com>
2023-02-01 23:58:56 +0300

    Добавляет плавную перемотку скролла наверх

--- src/components/up-button/up-button.tsx
+++ src/components/up-button/up-button.tsx
@@ -1,7 +1,9 @@
+import {animateScroll as scroll } from 'react-scroll';
+
 function UpButton (): JSX.Element {
 
   return (
-    <a className="up-btn" href="#header">
+    <a className="up-btn" href="#header" onClick={() => scroll.scrollToTop()}>
       <svg width={12} height={18} aria-hidden="true">
         <use xlinkHref="#icon-arrow2" />
       </svg>
