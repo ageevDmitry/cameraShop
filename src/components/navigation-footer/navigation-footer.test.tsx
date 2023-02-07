6ebc6d83cc0f697e48af2ecb654d6d4e406faa32
ageevDmitry <ageevdmitry2018@gmail.com>
2023-02-07 20:24:35 +0300

    Добавляет тесты для NavigationFooter

---
+++ src/components/navigation-footer/navigation-footer.test.tsx
@@ -0,0 +1,20 @@
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../../pages/history-route/history-route';
+import NavigationFooter from './navigation-footer';
+
+const history = createMemoryHistory();
+
+describe('Component: NavigationFooter', () => {
+  it('should render correctly', () => {
+    render(
+      <HistoryRouter history={history}>
+        <NavigationFooter/>
+      </HistoryRouter>
+    );
+
+    expect(screen.getByText(/Навигация/i)).toBeInTheDocument();
+    expect(screen.getByText(/Ресурсы/i)).toBeInTheDocument();
+    expect(screen.getByText(/Поддержка/i)).toBeInTheDocument();
+  });
+});
