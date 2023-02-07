0d3bd118e957845c5abd2e961ceb8ac5530a801a
ageevDmitry <ageevdmitry2018@gmail.com>
2023-02-07 21:31:09 +0300

    Добавляет тесты для ProductCharacteristics

---
+++ src/components/product-characteristics/product-characteristics.test.tsx
@@ -0,0 +1,30 @@
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../../pages/history-route/history-route';
+import ProductCharacteristics from './product-characteristics';
+import {product} from '../../mocks/mocks';
+import {ProductTab} from '../../const';
+
+const history = createMemoryHistory();
+
+describe('Component: ProductCharacteristics', () => {
+  it('should render correctly', () => {
+    render(
+      <HistoryRouter history={history}>
+        <ProductCharacteristics
+          currentTabControl={ProductTab.Characteristics}
+          vendorCode={product.vendorCode}
+          category={product.category}
+          type={product.type}
+          level={product.level}
+        />
+      </HistoryRouter>
+    );
+
+    expect(screen.getByText(product.vendorCode)).toBeInTheDocument();
+    expect(screen.getByText(product.category)).toBeInTheDocument();
+    expect(screen.getByText(product.type)).toBeInTheDocument();
+    expect(screen.getByText(product.level)).toBeInTheDocument();
+
+  });
+});
