076892c6ff968e1495d05ff525798f6986ee88e6
ageevDmitry <ageevdmitry2018@gmail.com>
2023-02-07 19:30:42 +0300

    Добавляет тесты для ModalReviewSuccess

---
+++ src/components/modal-review-success/modal-review-success.test.tsx
@@ -0,0 +1,56 @@
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import {Routes, Route} from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import HistoryRouter from '../../pages/history-route/history-route';
+import ModalReviewSuccess from './modal-review-success';
+import {product} from '../../mocks/mocks';
+
+const history = createMemoryHistory();
+
+describe('Component: ModalReviewSuccess', () => {
+
+  const setStateModalReview = jest.fn();
+
+  it('should render correctly', () => {
+    render(
+      <HistoryRouter history={history}>
+        <ModalReviewSuccess
+          product={product}
+          onClickCloseModalReviewSuccess={setStateModalReview}
+        />
+      </HistoryRouter>
+    );
+
+    expect(screen.getByText(/Спасибо за отзыв/i)).toBeInTheDocument();
+  });
+
+  it('should redirect to root url when user clicked to link', async () => {
+    history.push('/fake');
+
+    render(
+      <HistoryRouter history={history}>
+        <Routes>
+          <Route
+            path={`/product/${product.id}`}
+            element={<h1>This is main page</h1>}
+          />
+          <Route
+            path='*'
+            element={
+              <ModalReviewSuccess
+                product={product}
+                onClickCloseModalReviewSuccess={setStateModalReview}
+              />
+            }
+          />
+        </Routes>
+      </HistoryRouter>);
+
+    expect(screen.queryByText(/This is main page/i)).not.toBeInTheDocument();
+
+    await userEvent.click(screen.getByRole('link'));
+
+    expect(screen.getByText(/This is main page/i)).toBeInTheDocument();
+  });
+});
