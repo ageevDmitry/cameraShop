ff4dc49cb89a80100776f8e8ef5d4e0e377a5a9f
ageevDmitry <ageevdmitry2018@gmail.com>
2023-02-07 20:56:56 +0300

    Добавляет тесты для Pagination

---
+++ src/components/pagination/pagination.test.tsx
@@ -0,0 +1,32 @@
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../../pages/history-route/history-route';
+import Pagination from './pagination';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+
+const mockStore = configureMockStore();
+const store = mockStore();
+
+const history = createMemoryHistory();
+describe('Component: Pagination', () => {
+
+  const paginationCount = 5;
+  const currentCatalogPage = 1;
+
+  it('should render correctly', () => {
+
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <Pagination
+            paginationCount={paginationCount}
+            currentCatalogPage={currentCatalogPage}
+          />
+        </HistoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText(/1/i)).toBeInTheDocument();
+  });
+});
