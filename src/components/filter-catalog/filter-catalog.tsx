1b58321000de18e63856d5237388d79eacf5d244
ageevDmitry <ageevdmitry2018@gmail.com>
2023-02-25 23:26:30 +0300

    Добавляет блокировку кнопки Сбросить фильтры

--- src/components/filter-catalog/filter-catalog.tsx
+++ src/components/filter-catalog/filter-catalog.tsx
@@ -1,5 +1,12 @@
 import {useAppDispatch} from '../../hooks/useAppDispatch';
+import {useAppSelector} from '../../hooks/useAppSelector';
 import {cleanUpFilter} from '../../store/products-ui/products-ui';
+import {getCurrentCategory,
+  getCurrentType,
+  getCurrentLevel,
+  getCurrentMinPrice,
+  getCurrentMaxPrice
+} from '../../store/products-ui/selectors';
 import FilterPrice from '../filter-price/filter-price';
 import FilterCategory from '../filter-category/filter-category';
 import FilterType from '../filter-type/filter-type';
@@ -8,6 +15,11 @@ import FilterLevel from '../filter-level/filter-level';
 function FilterCatalog (): JSX.Element {
 
   const dispatch = useAppDispatch();
+  const currentType = useAppSelector(getCurrentType);
+  const currentCategory = useAppSelector(getCurrentCategory);
+  const currentLevel = useAppSelector(getCurrentLevel);
+  const currentMinPrice = useAppSelector(getCurrentMinPrice);
+  const currentMaxPrice = useAppSelector(getCurrentMaxPrice);
 
   return (
     <div className="catalog-filter">
@@ -21,6 +33,11 @@ function FilterCatalog (): JSX.Element {
           onClick={() => {
             dispatch(cleanUpFilter());
           }}
+          disabled={(currentType === null
+            && currentCategory === null
+            && currentLevel === null
+            && currentMinPrice === null
+            && currentMaxPrice === null)}
         >Сбросить фильтры
         </button>
       </form>
