3a8133a0c97d6588cee38ec9d4846f539bd1e6e7
ageevDmitry <ageevdmitry2018@gmail.com>
2023-02-26 16:31:53 +0300

    Добляет новый state для типов камер

--- src/components/filter-type/filter-type.tsx
+++ src/components/filter-type/filter-type.tsx
@@ -1,21 +1,28 @@
 import {useAppDispatch} from '../../hooks/useAppDispatch';
 import {useAppSelector} from '../../hooks/useAppSelector';
-import {getFilterTypeArray} from '../../utils';
+import {getFilterTypeArray,
+  getFilterTypeState} from '../../utils';
 import {getCurrentCategory,
   getCurrentType} from '../../store/products-ui/selectors';
 import {changeCurrentType} from '../../store/products-ui/products-ui';
 import {FilterCatalogType} from '../../const';
+import {useState, useEffect} from 'react';
 
 function FilterType (): JSX.Element {
 
   const dispatch = useAppDispatch();
   const currentCategory = useAppSelector(getCurrentCategory);
   const currentType = useAppSelector(getCurrentType);
+  const [flag, setFlag] = useState([false, false, false, false]);
+
+  useEffect(() => {
+    setFlag(getFilterTypeState(currentType));
+  }, [currentType]);
 
   return (
     <fieldset className="catalog-filter__block">
       <legend className="title title--h5">Тип камеры</legend>
-      {FilterCatalogType.map((item) => (
+      {FilterCatalogType.map((item, i) => (
         <div key={item.name} className="custom-checkbox catalog-filter__item">
           <div className="custom-checkbox catalog-filter__item">
             <label>
@@ -25,6 +32,7 @@ function FilterType (): JSX.Element {
                   dispatch(changeCurrentType({type: getFilterTypeArray(currentType, item.title)}));
                 }}
                 disabled={(currentCategory === item.disable)}
+                checked={flag[i]}
               /><span className="custom-checkbox__icon" /><span className="custom-checkbox__label">{item.title}</span>
             </label>
           </div>
--- src/utils.ts
+++ src/utils.ts
@@ -15,6 +15,15 @@ export const checkDisable = (currentCategory: string | null, currentType: string
   }
 };
 
+export const checkCheckBox = (currentType: string[] | null, itemChecked: string) => {
+  if (currentType !== null) {
+    const flag = currentType.includes(itemChecked);
+    return flag;
+  } else {
+    return false;
+  }
+};
+
 export const getFilterTypeArray = (array: string[] | null, type: string) => {
 
   if (array === null) {
@@ -98,4 +107,21 @@ export const getStringCurrentPrice = (currentPrice: number | null) => {
   } else {
     return String(currentPrice);
   }
+};
+
+export const getFilterTypeState = (currentType: string[] | null) => {
+
+  // const newCurrentState = currentState.slice();
+  const newCurrentState = [false, false, false, false];
+
+  if (currentType === null) {
+    return newCurrentState;
+  }
+
+  (currentType.includes('Цифровая')) ? newCurrentState[0] = true : newCurrentState[0] = false;
+  (currentType.includes('Плёночная')) ? newCurrentState[1] = true : newCurrentState[1] = false;
+  (currentType.includes('Моментальная')) ? newCurrentState[2] = true : newCurrentState[2] = false;
+  (currentType.includes('Коллекционная')) ? newCurrentState[3] = true : newCurrentState[3] = false;
+
+  return newCurrentState;
 };
