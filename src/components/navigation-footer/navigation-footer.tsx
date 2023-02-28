d596c7eff844d8beb4b244cc03c068dffad31489
ageevDmitry <ageevdmitry2018@gmail.com>
2023-02-28 21:15:09 +0300

    Исправляет навигацию в подвале

--- src/components/navigation-footer/navigation-footer.tsx
+++ src/components/navigation-footer/navigation-footer.tsx
@@ -1,12 +1,34 @@
-import {NAV_PAGES, RESOURCES_FOOTER_ITEMS, SUPPORT_FOOTER_ITEMS} from '../../const';
+import {NAV_PAGES,
+  RESOURCES_FOOTER_ITEMS,
+  SUPPORT_FOOTER_ITEMS} from '../../const';
+import {Link, generatePath} from 'react-router-dom';
+import {AppRoute, DEFAULT_CATALOG_PAGE} from '../../const';
+import {useAppSelector} from '../../hooks/useAppSelector';
+import {getCurrentCatalogPagePath} from '../../store/products-ui/selectors';
 
 function NavigationFooter (): JSX.Element {
+
+  const currentCatalogPagePath = useAppSelector(getCurrentCatalogPagePath);
+  const search = currentCatalogPagePath.search;
 
   return (
     <ul className="footer__nav">
       <li className="footer__nav-item">
         <p className="footer__title">Навигация</p>
         <ul className="footer__list">
+          <li className="footer__item">
+            <Link className="link"
+              to={{
+                pathname: generatePath(
+                  AppRoute.Catalog,
+                  {pageNumber: (currentCatalogPagePath.currentCatalogPage)
+                    ? String(currentCatalogPagePath.currentCatalogPage) : DEFAULT_CATALOG_PAGE}
+                ),
+                search
+              }}
+            >Каталог
+            </Link>
+          </li>
           {NAV_PAGES.map((item) => (
             <li key = {item.title} className="footer__item">
               <a className="link" href={item.href}>{item.title}</a>
