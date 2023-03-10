e178a666f61161a2170fd6a3c8bdde3cb983af3d
ageevDmitry <ageevdmitry2018@gmail.com>
2023-03-10 20:20:53 +0300

    Исправляет тесты filter-catalog.test

--- src/components/filter-catalog/filter-catalog.test.tsx
+++ src/components/filter-catalog/filter-catalog.test.tsx
@@ -2,15 +2,50 @@ import {render, screen} from '@testing-library/react';
 import {createMemoryHistory} from 'history';
 import HistoryRouter from '../history-route/history-route';
 import FilterCatalog from './filter-catalog';
+import {createAPI} from '../../services/api';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {State} from '../../types/state';
+import {Action} from 'redux';
+import {NameSpace} from '../../const';
+import {currentCatalogPagePath} from '../../types/ui';
+import {Provider} from 'react-redux';
+import {product, products, reviews} from '../../mocks/mocks';
+
+const api = createAPI();
+const middlewares = [thunk.withExtraArgument(api)];
+
+const mockStore = configureMockStore<
+State,
+Action<string>,
+ThunkDispatch<State, typeof api, Action>
+>(middlewares);
+
+const store = mockStore({
+  [NameSpace.ProductsUI]: {
+    currentCatalogPagePath: {} as currentCatalogPagePath,
+    currentMinPrice: null,
+    currentMaxPrice: null,
+  },
+  [NameSpace.ProductsData]: {
+    productDetail: product,
+    productsSimilar: products,
+    reviews: reviews,
+    isDataLoading: false,
+    isSuccess: false,
+  },
+});
 
 const history = createMemoryHistory();
 
 describe('Component: FilterCatalog', () => {
   it('should render correctly', () => {
     render(
-      <HistoryRouter history={history}>
-        <FilterCatalog/>
-      </HistoryRouter>
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <FilterCatalog/>
+        </HistoryRouter>
+      </Provider>
     );
 
     expect(screen.getByText(/Категория/i)).toBeInTheDocument();
