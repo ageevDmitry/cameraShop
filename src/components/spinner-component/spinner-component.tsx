26a422dcc28beb387a7542681ecb3262e5cd4d22
ageevDmitry <ageevdmitry2018@gmail.com>
2023-03-10 19:22:04 +0300

    Исправляет тесты spinner-component.test

--- src/components/spinner-component/spinner-component.tsx
+++ src/components/spinner-component/spinner-component.tsx
@@ -2,7 +2,7 @@ import styles from './spinner-component.module.css';
 
 function SpinnerComponent(): JSX.Element {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} data-testid="spinner-page">
       <div className={styles.loading}></div>
     </div>
   );
