23ae0a03f0bb3caee648fdd58706065713025d6f
ageevDmitry <ageevdmitry2018@gmail.com>
2023-03-10 19:55:04 +0300

    Исправляет тесты logo.test

--- src/components/logo/logo.test.tsx
+++ src/components/logo/logo.test.tsx
@@ -5,15 +5,42 @@ import {Routes, Route} from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import Logo from './logo';
 import {LogoType} from '../../const';
+import {createAPI} from '../../services/api';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {State} from '../../types/state';
+import {Action} from 'redux';
+import {NameSpace} from '../../const';
+import {currentCatalogPagePath} from '../../types/ui';
+import {Provider} from 'react-redux';
+
+const api = createAPI();
+const middlewares = [thunk.withExtraArgument(api)];
+
+const mockStore = configureMockStore<
+State,
+Action<string>,
+ThunkDispatch<State, typeof api, Action>
+>(middlewares);
+
+const store = mockStore({
+  [NameSpace.ProductsUI]: {
+    currentCatalogPagePath: {} as currentCatalogPagePath,
+    currentMinPrice: null,
+    currentMaxPrice: null,
+  },
+});
 
 const history = createMemoryHistory();
 
 describe('Component: Logo', () => {
   it('should render correctly', () => {
     render(
-      <HistoryRouter history={history}>
-        <Logo typeComponent = {LogoType.Header}/>
-      </HistoryRouter>
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <Logo typeComponent = {LogoType.Header}/>
+        </HistoryRouter>
+      </Provider>
     );
 
     expect(screen.getByRole('link')).toBeInTheDocument();
@@ -23,20 +50,23 @@ describe('Component: Logo', () => {
     history.push('/fake');
 
     render(
-      <HistoryRouter history={history}>
-        <Routes>
-          <Route
-            path="/"
-            element={<h1>This is main page</h1>}
-          />
-          <Route
-            path='*'
-            element={
-              <Logo typeComponent = {LogoType.Header}/>
-            }
-          />
-        </Routes>
-      </HistoryRouter>);
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <Routes>
+            <Route
+              path="/catalog/page_1/"
+              element={<h1>This is main page</h1>}
+            />
+            <Route
+              path='*'
+              element={
+                <Logo typeComponent = {LogoType.Header}/>
+              }
+            />
+          </Routes>
+        </HistoryRouter>
+      </Provider>
+    );
 
     expect(screen.queryByText(/This is main page/i)).not.toBeInTheDocument();
 
