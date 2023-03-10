3c1333e52f8133f02956122ce50914cbada0c665
ageevDmitry <ageevdmitry2018@gmail.com>
2023-03-10 20:13:52 +0300

    Испраляет тесты footer.test

--- src/components/footer/footer.test.tsx
+++ src/components/footer/footer.test.tsx
@@ -2,15 +2,42 @@ import {render, screen} from '@testing-library/react';
 import {createMemoryHistory} from 'history';
 import HistoryRouter from '../history-route/history-route';
 import Footer from './footer';
+import {createAPI} from '../../services/api';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {State} from '../../types/state';
+import {Action} from 'redux';
+import {NameSpace} from '../../const';
+import {currentCatalogPagePath} from '../../types/ui';
+import {Provider} from 'react-redux';
+
+const api = createAPI();
+const middlewares = [thunk.withExtraArgument(api)];
+
+const mockStore = configureMockStore<
+State,
+Action<string>,
+ThunkDispatch<State, typeof api, Action>
+>(middlewares);
+
+const store = mockStore({
+  [NameSpace.ProductsUI]: {
+    currentCatalogPagePath: {} as currentCatalogPagePath,
+    currentMinPrice: null,
+    currentMaxPrice: null,
+  },
+});
 
 const history = createMemoryHistory();
 
 describe('Component: Footer', () => {
   it('should render correctly', () => {
     render(
-      <HistoryRouter history={history}>
-        <Footer/>
-      </HistoryRouter>
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <Footer/>
+        </HistoryRouter>
+      </Provider>
     );
 
     expect(screen.getByText(/Интернет-магазин фото- и видеотехники/i)).toBeInTheDocument();
