074d200911e31c07fb0ec44f28036419513d99ef
Dmitry Ageev <ageevdmitry2018@gmail.com>
2023-04-15 22:03:22 +0300

    Исправляет тест Product-Similar

--- src/components/products-similar/products-similar.test.tsx
+++ src/components/products-similar/products-similar.test.tsx
@@ -1,19 +1,54 @@
 import {render, screen} from '@testing-library/react';
 import {createMemoryHistory} from 'history';
 import HistoryRouter from '../history-route/history-route';
-import {products} from '../../mocks/mocks';
 import ProductSimilar from './products-similar';
+import {createAPI} from '../../services/api';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import {Action} from 'redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {State} from '../../types/state';
+import {NameSpace} from '../../const';
+import {Provider} from 'react-redux';
+import {currentCatalogPagePath} from '../../types/ui';
+import {product, products, reviews, productsCart} from '../../mocks/mocks';
+
+const api = createAPI();
+const middlewares = [thunk.withExtraArgument(api)];
+
+const mockStore = configureMockStore<
+State,
+Action<string>,
+ThunkDispatch<State, typeof api, Action>
+>(middlewares);
+
+const store = mockStore({
+  [NameSpace.ProductsData]: {
+    productDetail: product,
+    productsSimilar: products,
+    productsCart: productsCart,
+    reviews: reviews,
+    isDataLoading: false,
+    isSuccess: false,
+  },
+  [NameSpace.ProductsUI]: {
+    currentCatalogPagePath: {} as currentCatalogPagePath,
+    currentMinPrice: null,
+    currentMaxPrice: null,
+  },
+});
 
 const history = createMemoryHistory();
 
 describe('Component: ProductSimilar', () => {
   it('should render correctly', () => {
     render(
-      <HistoryRouter history={history}>
-        <ProductSimilar
-          productsSimilar={products}
-        />
-      </HistoryRouter>
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <ProductSimilar
+            productsSimilar={products}
+          />
+        </HistoryRouter>
+      </Provider>
     );
 
     expect(screen.getByText(/Похожие товары/i)).toBeInTheDocument();
